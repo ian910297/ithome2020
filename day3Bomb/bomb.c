aa8469180ef51efeda1b76fe174736270ac7d4b6
Chungyi Chi <demonic@csie.io>
2020-09-22 01:39:43 +0000

    add phase_2

--- day3Bomb/bomb.c
+++ day3Bomb/bomb.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 void phase_1() {
     int x=12;
@@ -7,12 +8,24 @@ void phase_1() {
       system("google-chrome --incognito pornhub.com");
       exit(1);
     } else {
-      system("google-chrome --incognito google.com");
+      printf("pass phase_1\n");
     }
+}
+
+void phase_2() {
+  char text[] = "pornhub";
+  char input[100];
+  scanf("%s", input);
+  if(strcmp(input, text)==0) {// match
+    printf("pass phase_2\n");
+  } else {
+    exit(1);
+  }
 }
 
 int main() {
     phase_1();
+    phase_2();
 
     return 0;
 }
